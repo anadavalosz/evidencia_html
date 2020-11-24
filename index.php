9c5caada084751ae126a3798c7278d50c7b211e6
anadavalosz <74082255+anadavalosz@users.noreply.github.com>
2020-11-23 22:19:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
-<?php
+<?php include_once("./html/index.html"); ?>
+<?php include_once("./html/frameset.html"); ?>
+<?php //include_once("./html/encabezado_pagina1.html"); ?>
+<?php //include_once("./html/frameset2.html"); ?>
+<?php //include_once("./html/pagina2.html"); ?>
+<?php //include_once("./html/carousel.html"); ?>
 
-incluide_once("/html/frameset.html")
-
-?>
